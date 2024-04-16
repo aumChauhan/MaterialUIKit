df80467fc286b52416eae310b1250c3878cc4e1f
Aum Chauhan <aumchauhan175@gmail.com>
2024-04-16 15:52:04 +0530

    Added MUIBlankView

---
+++ Sources/MaterialUIKit/Components/Navigation And Structure/MUIBlankView.swift
@@ -0,0 +1,39 @@
+//
+// MUIView.swift
+// MaterialUIKit: https://github.com/aumChauhan/MaterialUIKit.git
+//
+// Author: Aum Chauhan
+// Created On: 16/04/24
+//
+
+import SwiftUI
+
+/// A blank-view with default MaterialUIKit styling including background and padding.
+@available(iOS 15.0, *)
+public struct MUIBlankView<Content>: View where Content: View {
+    
+    // MARK: - Properties
+    
+    let content: Content
+    
+    // MARK: - Initializer
+    
+    /// Creates a empty view with default MaterialUIKit styling including background and padding.
+    /// - Parameter content: The content to be wrapped in the blank view.
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    // MARK: - View Body
+    
+    public var body: some View {
+        ZStack {
+            // Background
+            MaterialUIKit.tint.primaryBackground().ignoresSafeArea(.all)
+            
+            // Content
+            content
+                .padding(MUIConstants.insideMargin)
+        }
+    }
+}
