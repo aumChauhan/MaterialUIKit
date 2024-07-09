f5bd82d4ce11daf3808856ccf941961f0a11f888
Aum Chauhan <aumchauhan175@gmail.com>
2024-07-09 19:48:54 +0530

    Enhancement:  Date & time picker now follow configuration constants and improve animations

--- Sources/MaterialUIKit/UI Components/Form Controls/MaterialDatePicker.swift
+++ Sources/MaterialUIKit/UI Components/Form Controls/MaterialDatePicker.swift
@@ -16,15 +16,15 @@ extension View {
     ///   - isPresented: A binding to control the presentation state of the date picker.
     ///   - selection: A binding to manage the selected date.
     public func materialDatePicker(isPresented: Binding<Bool>, selection: Binding<Date>) -> some View {
-        return self.modifier(MaterialDatePickerModifier(isPresented: isPresented, selection: selection))
+        return self.modifier(MaterialDatePickerViewModifier(isPresented: isPresented, selection: selection))
     }
     
 }
 
-// MARK: - MaterialDatePickerModifier
+// MARK: - MaterialDatePickerViewModifier
 
 /// A view modifier that adds date picker presentation behavior to any view.
-fileprivate struct MaterialDatePickerModifier: ViewModifier {
+fileprivate struct MaterialDatePickerViewModifier: ViewModifier {
     
     // MARK: - Properties
     
@@ -43,10 +43,10 @@ fileprivate struct MaterialDatePickerModifier: ViewModifier {
 // MARK: - MaterialDatePickerView
 
 /// A Material Design styled date picker.
-@available(iOS 15.0, *)
 fileprivate struct MaterialDatePickerView: View {
     
     // MARK: - Properties
+    
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @Environment(\.verticalSizeClass) private var verticalSizeClass
     
@@ -58,63 +58,43 @@ fileprivate struct MaterialDatePickerView: View {
     
     var body: some View {
         ZStack {
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: MaterialUIKit.configuration.stackSpacing) {
                 if horizontalSizeClass == .compact && verticalSizeClass == .regular {
-                    // For portrait mode
                     portraitDatePicker()
                 } else if horizontalSizeClass == .compact && verticalSizeClass == .compact {
-                    // For landscope mode
                     landscapeDatePicker()
                 } else {
                     portraitDatePicker()
                 }
             }
-            .padding(20)
-            .background(.materialTonal)
-            .cornerRadius(MaterialUIKitConstants.cornerRadius)
-            .padding(0.8)
-            .background(.materialSecondaryTitle.opacity(0.5))
-            .cornerRadius(MaterialUIKitConstants.cornerRadius)
-            .scaleEffect(animationFlag ? 1 : 0)
-            .padding(20)
+            .frame(width: UIScreen.main.bounds.width/1.3)
+            .primaryBackground()
+            .scaleEffect(animationFlag ? 1 : 1.1)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black.opacity(0.45))
-        .opacity(animationFlag ? 1 : 0)
-        .onChangeWithFallback(of: isPresented) { oldValue, newValue in
-            withAnimation(.bouncy) {
-                animationFlag = isPresented
-            }
-        }
+        .modalBackdrop(isPresented: $isPresented, animationFlag: $animationFlag)
     }
-    
-    // MARK: - Helpers
     
     /// Returns the dismiss button bar for the date picker.
     private func dismissDatePicker() -> some View {
-        HStack {
-            Spacer()
-            
-            Button {
-                withAnimation {
-                    isPresented.toggle()
-                }
-            } label: {
-                Text("OK")
-                    .textStyledBackground(10)
+        Button {
+            withMaterialAnimation {
+                isPresented.toggle()
             }
+        } label: {
+            Text("OK")
+                .textStyledBackground(MaterialUIKit.configuration.verticalPadding)
         }
+        .align(.trailing)
     }
     
     /// Returns the date picker view for portrait mode.
     private func portraitDatePicker() -> some View {
-        VStack(alignment: .leading, spacing: 10) {
+        VStack(alignment: .leading) {
             Text("\(selection.formattedMUIDate())")
-                .font(.title2)
+                .font(.title)
+                .padding(.top, MaterialUIKit.configuration.verticalPadding)
                 .fontWeightWithFallback(.medium)
                 .foregroundStyle(.materialPrimaryTitle)
-                .padding(.top, 5)
-                .padding(.leading, 10)
             
             MaterialDivider()
             
@@ -122,21 +102,18 @@ fileprivate struct MaterialDatePickerView: View {
                 .datePickerStyle(.graphical)
                 .tint(.materialAccent)
             
-            MaterialDivider()
-            
             dismissDatePicker()
         }
     }
     
     /// Returns the date picker view for landscape mode.
     private func landscapeDatePicker() -> some View {
-        VStack(alignment: .leading, spacing: 10) {
+        VStack(alignment: .leading) {
             Text("\(selection.formattedMUIDate())")
-                .font(.title2)
+                .font(.title)
+                .padding(.top, MaterialUIKit.configuration.verticalPadding)
                 .fontWeightWithFallback(.medium)
                 .foregroundStyle(.materialPrimaryTitle)
-                .padding(.top, 5)
-                .padding(.leading, 10)
             
             MaterialDivider()
             
@@ -146,10 +123,8 @@ fileprivate struct MaterialDatePickerView: View {
                     .tint(.materialAccent)
             }
             
-            MaterialDivider()
-            
             dismissDatePicker()
         }
-        .frame(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.height / 1.2)
+        .frame(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.height/1.2)
     }
 }
--- Sources/MaterialUIKit/UI Components/Form Controls/MaterialTimePicker.swift
+++ Sources/MaterialUIKit/UI Components/Form Controls/MaterialTimePicker.swift
@@ -10,25 +10,25 @@ import SwiftUI
 
 extension View {
     
-    /// Presents a Material Design styled time picker.
+    /// Presents a Material UI style time picker.
     ///
     /// - Parameters:
     ///   - isPresented: A binding to control the presentation of the time picker.
     ///   - selection: A binding to manage the selected date.
-    public func muiTimePicker(isPresented: Binding<Bool>, selection: Binding<Date>) -> some View {
-        return self.modifier(MaterialTimePickerModifier(isPresented: isPresented, selection: selection))
+    public func materialTimePicker(isPresented: Binding<Bool>, selection: Binding<Date>) -> some View {
+        return self.modifier(MaterialTimePickerViewModifier(isPresented: isPresented, selection: selection))
     }
 }
 
-// MARK: - MaterialTimePickerModifier
+// MARK: - MaterialTimePickerViewModifier
 
 /// A view modifier that adds time picker to any view.
-private struct MaterialTimePickerModifier: ViewModifier {
+fileprivate struct MaterialTimePickerViewModifier: ViewModifier {
     
     // MARK: - Properties
     
-    @Binding internal var isPresented: Bool
-    @Binding internal var selection: Date
+    @Binding var isPresented: Bool
+    @Binding var selection: Date
     
     // MARK: - Body
     
@@ -42,22 +42,22 @@ private struct MaterialTimePickerModifier: ViewModifier {
 // MARK: - MaterialTimePickerView
 
 /// A Material UI styled time picker.
-@available(iOS 15.0, *)
-private struct MaterialTimePickerView: View {
+fileprivate struct MaterialTimePickerView: View {
     
     // MARK: - Properties
+    
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @Environment(\.verticalSizeClass) private var verticalSizeClass
     
-    @Binding internal var isPresented: Bool
-    @Binding internal var selection: Date
+    @Binding var isPresented: Bool
+    @Binding var selection: Date
     @State private var animationFlag: Bool = false
     
     // MARK: - View Body
     
-    public var body: some View {
+    var body: some View {
         ZStack {
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: MaterialUIKit.configuration.stackSpacing) {
                 if horizontalSizeClass == .compact && verticalSizeClass == .regular {
                     portraitDatePicker()
                 } else if horizontalSizeClass == .compact && verticalSizeClass == .compact {
@@ -66,92 +66,64 @@ private struct MaterialTimePickerView: View {
                     portraitDatePicker()
                 }
             }
-            .padding(20)
-            .background(.materialTonal)
-            .cornerRadius(MaterialUIKitConstants.cornerRadius)
-            .padding(0.8)
-            .background(.materialSecondaryTitle.opacity(0.4))
-            .cornerRadius(MaterialUIKitConstants.cornerRadius)
-
-            .scaleEffect(animationFlag ? 1 : 0)
-            .padding(20)
+            .frame(width: UIScreen.main.bounds.width/1.3)
+            .primaryBackground()
+            .scaleEffect(animationFlag ? 1 : 1.1)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.black.opacity(0.45))
-        .opacity(animationFlag ? 1 : 0)
-        .onChangeWithFallback(of: isPresented) { oldValue, newValue in
-            withAnimation(.bouncy) {
-                animationFlag = isPresented
+        .modalBackdrop(isPresented: $isPresented, animationFlag: $animationFlag)
+    }
+    
+    /// Returns the dismiss button bar for the time picker.
+    private func dismissDatePicker() -> some View {
+        Button {
+            withMaterialAnimation {
+                isPresented.toggle()
             }
+        } label: {
+            Text("OK")
+                .textStyledBackground()
+        }
+        .align(.trailing)
+    }
+    
+    /// Returns the time picker view for portrait mode.
+    private func portraitDatePicker() -> some View {
+        VStack {
+            Text("\(selection.formatted(date: .omitted, time: .shortened))")
+                .font(.title)
+                .padding(.top, MaterialUIKit.configuration.verticalPadding)
+                .fontWeightWithFallback(.medium)
+                .foregroundStyle(.materialPrimaryTitle)
+            
+            MaterialDivider()
+            
+            DatePicker("", selection: $selection, displayedComponents: .hourAndMinute)
+                .datePickerStyle(.wheel)
+                .tint(.materialAccent)
+            
+            dismissDatePicker()
         }
     }
     
-    // MARK: - Helpers
-
-    /// Returns the dismiss button bar for the time picker.
-        private func dismissDatePicker() -> some View {
-            HStack {
-                Spacer()
-                
-                Button {
-                    withAnimation(.bouncy) {
-                        isPresented.toggle()
-                    }
-                } label: {
-                    Text("OK")
-                        .textStyledBackground(10)
-                }
-            }
-        }
-        
-        /// Returns the time picker view for portrait mode.
-        private func portraitDatePicker() -> some View {
-            VStack(alignment: .leading, spacing: 10) {
-                // Time
-                Text("\(selection.formatted(date: .omitted, time: .shortened))")
-                    .font(.title2)
-                    .fontWeightWithFallback(.medium)
-                    .foregroundStyle(.materialPrimaryTitle)
-                    .padding(.top, 5)
-                    .padding(.leading, 10)
-                
-                MaterialDivider()
-                
-                // SwiftUI's default timepicker
+    /// Returns the time picker view for landscape mode.
+    private func landscapeDatePicker() -> some View {
+        VStack {
+            Text("\(selection.formatted(date: .omitted, time: .shortened))")
+                .font(.title)
+                .padding(.top, MaterialUIKit.configuration.verticalPadding)
+                .fontWeightWithFallback(.medium)
+                .foregroundStyle(.materialPrimaryTitle)
+            
+            MaterialDivider()
+            
+            ScrollView {
                 DatePicker("", selection: $selection, displayedComponents: .hourAndMinute)
                     .datePickerStyle(.wheel)
                     .tint(.materialAccent)
-                
-                MaterialDivider()
-                
-                dismissDatePicker()
             }
+            
+            dismissDatePicker()
         }
-        
-        /// Returns the time picker view for landscape mode.
-        private func landscapeDatePicker() -> some View {
-            VStack(alignment: .leading, spacing: 10) {
-                // Time
-                Text("\(selection.formatted(date: .omitted, time: .shortened))")
-                    .font(.title2)
-                    .fontWeightWithFallback(.medium)
-                    .foregroundStyle(.materialPrimaryTitle)
-                    .padding(.top, 5)
-                    .padding(.leading, 10)
-                
-                MaterialDivider()
-                
-                ScrollView {
-                    // SwiftUI's default timepicker
-                    DatePicker("", selection: $selection, displayedComponents: .hourAndMinute)
-                        .datePickerStyle(.wheel)
-                        .tint(.materialAccent)
-                }
-                
-                MaterialDivider()
-                
-                dismissDatePicker()
-            }
-            .frame(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.height / 1.2)
-        }
+        .frame(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.height/1.2)
+    }
 }
